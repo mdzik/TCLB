dcd73c8cc4902a626ef30702fb6c80637062011d
ggruszczynski <ggruszczynski@gmail.com>
2019-01-25 21:00:07 +0100

    fixing BC

--- models/heat/heat_cm_d2q9/Dynamics.R
+++ models/heat/heat_cm_d2q9/Dynamics.R
@@ -1,33 +1,11 @@
 source("lib/lattice.R")
 
-# Density - table of variables of LB Node to stream
+#  Density - table of variables of LB Node to stream
 #  name - variable name to stream
 #  dx,dy,dz - direction of streaming
 #  comment - additional comment
 
-# AddDensity( name="f[0]", dx= 0, dy= 0, group="f")
-# AddDensity( name="f[1]", dx= 1, dy= 0, group="f")
-# AddDensity( name="f[2]", dx= 0, dy= 1, group="f")
-# AddDensity( name="f[3]", dx=-1, dy= 0, group="f")
-# AddDensity( name="f[4]", dx= 0, dy=-1, group="f")
-# AddDensity( name="f[5]", dx= 1, dy= 1, group="f")
-# AddDensity( name="f[6]", dx=-1, dy= 1, group="f")
-# AddDensity( name="f[7]", dx=-1, dy=-1, group="f")
-# AddDensity( name="f[8]", dx= 1, dy=-1, group="f")
-
-
-# AddDensity( name="h[0]", dx= 0, dy= 0, group="h")
-# AddDensity( name="h[1]", dx= 1, dy= 0, group="h")
-# AddDensity( name="h[2]", dx= 0, dy= 1, group="h")
-# AddDensity( name="h[3]", dx=-1, dy= 0, group="h")
-# AddDensity( name="h[4]", dx= 0, dy=-1, group="h")
-# AddDensity( name="h[5]", dx= 1, dy= 1, group="h")
-# AddDensity( name="h[6]", dx=-1, dy= 1, group="h")
-# AddDensity( name="h[7]", dx=-1, dy=-1, group="h")
-# AddDensity( name="h[8]", dx= 1, dy=-1, group="h")
-
-
-# recznie: AddDensity( name="f[0]", dx= 0, dy= 0, group="f")
+# by hand: AddDensity( name="f[0]", dx= 0, dy= 0, group="f")
 AddDensity(
 	name = paste("f[",1:9-1,"]",sep=""),
 	dx   = d2q9[,1],
@@ -45,6 +23,7 @@ AddDensity(
 	group="h"
 )
 
+# 	Outputs:
 AddQuantity( name="Rho" )
 AddQuantity( name="T" )
 AddQuantity( name="RawU", vector=T )
@@ -52,7 +31,8 @@ AddQuantity( name="U", vector=T )
 
 #	Inputs: Flow Properties
 AddSetting(name="VelocityX", default=0.0, comment='inlet/outlet/init x-velocity component', zonal=TRUE)
-AddSetting(name="GravitationY", default=0.0, comment='applied (rho)*GravitationY')
+AddSetting(name="GravitationY", default=0.0, comment='applied rho*GravitationY')
+AddSetting(name="GravitationX", default=0.0, comment='applied rho*GravitationX')
 AddSetting(name="omega_nu", comment='inverse of viscous relaxation time', default=1.0)
 AddSetting(name="nu", omega_nu='1.0/(3*nu+0.5)',  default=0.16666666,  comment='kinematic viscosity')
 AddSetting(name="omega_bulk", comment='inverse of bulk relaxation time', default=1.0)
@@ -62,7 +42,7 @@ AddSetting(name="bulk_visc", omega_bulk='1.0/(3*bulk_visc+0.5)',  comment='bulk 
 AddSetting(name="omega_k", default=1.0 , comment='inverse of thermal relaxation time')
 AddSetting(name="k", omega_k='1.0/(3*k+0.5)', default=0.16666666, comment='thermal conductivity (W/(m·K))')
 AddSetting(name="InitTemperature", default=0, comment='Initial/Inflow temperature distribution', zonal=T)
-AddSetting(name="BoussinesqCoeff", default=0.0, comment='Boussinesq force: coefficient of thermal expansion --> Fb_Y = -gravitation*coeff*dT')
+AddSetting(name="BoussinesqCoeff", default=0.0, comment='BoussinesqCoeff=rho_0*thermal_exp_coeff')
 
 
 #	Boundary things
