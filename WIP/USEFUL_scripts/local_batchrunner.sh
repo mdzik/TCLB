1d58d07fb1825d417f5bdd3368030a131569659a
ggruszczynski <ggruszczynski@gmail.com>
2020-02-21 13:30:28 +0100

    d3q27q7 examples

--- WIP/USEFUL_scripts/local_batchrunner.sh
+++ WIP/USEFUL_scripts/local_batchrunner.sh
@@ -6,7 +6,7 @@ set -e
 DIRNAME=$1
 
 TCLB_PATH="~/GITHUB/TCLB/CLB"
-MODEL="d3q27q27_cm_cht_IBB"
+MODEL="d3q27q27_cm_cht_OutFlowNeumann_IBB"
 
 
 if test -d $DIRNAME
@@ -27,3 +27,4 @@ else
 fi
 
 echo -e "\n\n All jobs from $DIRNAME launched!"
+
