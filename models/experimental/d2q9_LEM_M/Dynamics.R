a1193b1d2dab34223fd9dd42e37b45a0e8f05289
ggruszczynski <ggruszczynski@gmail.com>
2020-06-29 20:09:07 +0200

    sig interpolation

--- models/experimental/d2q9_LEM_M/Dynamics.R
+++ models/experimental/d2q9_LEM_M/Dynamics.R
@@ -36,12 +36,14 @@ AddGlobal(name="NoOfInfected",        comment='Number of Infected individuals', 
 AddGlobal(name="NoOfRecovered",       comment='Number of Recovered individuals', unit="")
 
 #	Boundary things:
-AddNodeType(name="DirichletEQ", group="BOUNDARY")
-AddNodeType(name="ImageReader", group="IMAGE") 
-AddNodeType(name="CROSSMRT",	group="COLLISION")
+AddNodeType(name="DirichletEQ",     group="BOUNDARY")
+AddNodeType(name="ImageReader",     group="IMAGE") 
+AddNodeType(name="CROSSMRT",	    group="COLLISION")
+AddNodeType(name="CROSSMRT_FOI",	group="COLLISION")
 
 
-#   Inputs: Flow Properties
+
+# Inputs: Flow Properties
 # https://en.wikipedia.org/wiki/Compartmental_models_in_epidemiology#The_SIR_model
 # Between S and I, the transition rate is βI/N, where β is the average number of contacts per person per time, 
 # multiplied by the probability of disease transmission in a contact between a susceptible and an infectious subject,
@@ -54,21 +56,24 @@ AddNodeType(name="CROSSMRT",	group="COLLISION")
 
 AddSetting(name="sir_beta",                 default=0.0,        comment="s2i constant -> infection rate")
 AddSetting(name="sir_gamma",                default=0.0,        comment="i2r constant -> 1/gamma is the mean infective period")
-AddSetting(name="diffusivity_s",              default=0.16666666, comment='spacial diffusivity for the fraction of suspected',	zonal=T)
-AddSetting(name="diffusivity_i",              default=0.16666666, comment='spacial diffusivity for the fraction of infected',	zonal=T)
-AddSetting(name="diffusivity_r",              default=0.16666666, comment='spacial diffusivity for the fraction of recovered',	zonal=T)
-AddSetting(name="stability_enhancement",    default=1.0,        comment='magic stability enhancement')
+AddSetting(name="diffusivity_s",            default=0.16666666, comment='spacial diffusivity for the fraction of suspected', zonal=T)
+AddSetting(name="diffusivity_i",            default=0.16666666, comment='spacial diffusivity for the fraction of infected',	zonal=T)
+AddSetting(name="diffusivity_r",            default=0.16666666, comment='spacial diffusivity for the fraction of recovered', zonal=T)
+AddSetting(name="sigma2_tweaker",           default=1.0,        comment='to modify the variance')
 AddSetting(name="omega_turb_multiplicator", default=0.0,        comment="extra diffusivity in locations with high population density")
 
-AddSetting(name="infectious_radius", default=0.4082482,        comment="used in cross-MRT model")
+AddSetting(name="infectious_radius",        default=1,         comment="used in cross-MRT model")
+AddSetting(name="cross_omega_limiter_low",  default=1.,        comment="used in cross-MRT model")
+AddSetting(name="cross_omega_limiter_high", default=2.,        comment="used in cross-MRT model")
 
-AddSetting(name="Init_PopulationDensity",    zonal=TRUE)
-AddSetting(name="Init_S_Fraction",      zonal=TRUE)
-AddSetting(name="Init_I_Fraction",      zonal=TRUE)
-AddSetting(name="Init_R_Fraction",      zonal=TRUE)
+
+AddSetting(name="Init_PopulationDensity",   zonal=TRUE)
+AddSetting(name="Init_S_Fraction",          zonal=TRUE)
+AddSetting(name="Init_I_Fraction",          zonal=TRUE)
+AddSetting(name="Init_R_Fraction",          zonal=TRUE)
 
 #	CFD enhancements ;)
-AddField(name="populationDensity", stencil2d=1)
-AddNodeType(name="Smoothing", group="ADDITIONALS")  #  To smooth population density during initialization.
+AddField(name="populationDensity",          stencil2d=1)
+AddNodeType(name="Smoothing",               group="ADDITIONALS")  #  To smooth population density during initialization.
 AddSetting(name="population_smoothing")  #  To smooth population density during initialization.
 
